d0d661b4058fad2396a29c9d2200760c9f579207
Mat Tyndall <mat.tyndall@gmail.com>
2012-01-01 23:14:14 -0800

    compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ SampleApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +46,7 @@ SampleApp::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   #added to precompile blueprint CSS
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css )
+  #config.assets.precompile += %w( blueprint/screen.css blueprint/print.css blueprint/ie.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
